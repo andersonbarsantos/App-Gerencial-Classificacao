4959a14f99c4e426b2d5d96bd9bb554a5deeb5c3
Anderson Santos <anderson.santos@confitec.com.br>
2020-02-07 19:01:41 -0300

    Adicionar arquivos de projeto.

---
+++ Gerencial-431/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gerencial_431.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
