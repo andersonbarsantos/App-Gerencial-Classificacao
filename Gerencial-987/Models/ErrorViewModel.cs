bcf24f4e4ac15dc082741090f744c7a8161bc4ca
Anderson Santos <anderson.santos@confitec.com.br>
2020-02-07 19:09:02 -0300

    987

---
+++ Gerencial-987/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gerencial_987.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
